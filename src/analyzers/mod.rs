a7ca50d00bd8c7d16eedbf89f19bb34ba3fef428
pointerliu <zizhen_liu@nudt.edu.cn>
2025-02-14 23:45:12 +0800

    analyzer: add ref vecs.

--- src/analyzers/mod.rs
+++ src/analyzers/mod.rs
@@ -4,7 +4,7 @@ pub trait TrendingAnalyzer {
     type Raw;
     type Ctx;
 
-    fn problems(&self, data: Vec<(Self::Raw, Self::Ctx)>) -> Vec<(String, Vec<Self::Raw>)>;
-    fn techniques(&self, data: Vec<(Self::Raw, Self::Ctx)>) -> Vec<(String, Vec<Self::Raw>)>;
-    fn fields(&self, data: Vec<(Self::Raw, Self::Ctx)>) -> Vec<(String, Vec<Self::Raw>)>;
+    fn problems(&self, data: &Vec<(Self::Raw, Self::Ctx)>) -> Vec<(String, Vec<Self::Raw>)>;
+    fn techniques(&self, data: &Vec<(Self::Raw, Self::Ctx)>) -> Vec<(String, Vec<Self::Raw>)>;
+    fn fields(&self, data: &Vec<(Self::Raw, Self::Ctx)>) -> Vec<(String, Vec<Self::Raw>)>;
 }
